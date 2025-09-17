37e3edb48571d3719b620ace09d2acbdc5d5a1d0
Евгений Лазарев <evgeniy.lazarev@mstarproject.com>
2025-09-17 15:39:04 +0300

    убираем лишнее

--- app/Http/Controller.php
+++ app/Http/Controller.php
@@ -2,22 +2,43 @@
 
 namespace App\Http;
 
-use App\Api\ApiController;
 use App\Core\Http\Request;
 use App\Core\Http\Response;
 use App\Exception\TwigRenderException;
+use App\Validation\RequestValidator;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
 use Twig\TwigFunction;
 use Throwable;
 
-abstract class Controller extends ApiController
+abstract class Controller
 {
-//    public function __construct(public Request $request, public Response $response) {
-//
-//
-//        parent::__construct($request, $this->response);
-//    }
+    protected array $oldData = [];
+    public array $errors = [];
+
+    // применим фишки php8 с автоматическим созданием атрибута
+    // через указание в параметрах конструктора
+    public function __construct(public Request $request, public Response $response) {
+        $this->oldData = $this->request->post ?? [];
+    }
+    public function json(array $payload = [], int $code = 200) : Response
+    {
+        // сделаем вывод json красивым в Chrome браузере
+        $body = json_encode($payload, JSON_UNESCAPED_UNICODE | JSON_PRETTY_PRINT);
+
+        return $this->response
+            ->withHeader('Content-Type', 'application/json; charset=utf-8')
+            ->withStatus($code)
+            ->write($body);
+    }
+
+    public function validate(array $data, array $validation) : array
+    {
+        [$result, $errors] = RequestValidator::validate($data, $validation);
+
+        $this->errors = $errors;
+        return $result;
+    }
 
     // такие контроллеры у нас используются для выдачи html
     // по этому здесь и подключаем шаблонизатор
